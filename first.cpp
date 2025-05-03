7651bbafaa21fc9a76c66b351bbdc22465f5f70f
Sandhya Lekshmana Kammath <sandhya_kammath@yahoo.co.in>
2025-05-03 15:12:47 +0100

    Sample Linux Programmes

---
+++ first.cpp
@@ -0,0 +1,26 @@
+///////////////////////
+//first.cpp
+//
+// A hello world program using GNU C/C++ under Linux
+//
+// g++ first.cpp => a.out
+// if you want to give another name (say test.exe ) for
+// the executable
+//
+// g++ -otest.exe first.cpp
+//
+// ./test.exe
+// test.exe will give you error , to avoid the error
+// reset the enviornment variable
+//  export PATH=.:$PATH
+// test.exe ( will print hello world on to the console )
+// Console output
+// -----------------------
+//Hello world
+
+
+#include <stdio.h>
+//---- user entry point 
+int main( int argc , char **argv ){
+  	printf("Hello world \n" );
+} 
